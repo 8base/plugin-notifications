b13037160a78b6d305d1e12616fbf6cc7e350a8b
Osipov Vladimir <zouxuoz@gmail.com>
2020-02-10 14:10:11 +0300

    chore: rid of unnecessary mark as read mutation

--- src/resolvers/userNotificationMarkAsRead/queries.ts
+++
@@ -1,9 +0,0 @@
-import gql from 'graphql-tag';
-
-export const USER_NOTIFICATION_UPDATE_MUTATION = gql`
-  mutation UserNotificationUpdate($data: UserNotificationUpdateInput!) {
-    userNotificationUpdate(data: $data) {
-      id
-    }
-  }
-`;
